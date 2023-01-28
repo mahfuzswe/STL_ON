bcd66a1d4626048453609f3399f6f0ebbdea9698
Invincible-21 <shantobd1969@gmail.com>
2023-01-29 01:11:11 +0600

    3d vector

---
+++ Striver/Lecture-01/07.cpp
@@ -0,0 +1,29 @@
+// ** 3D Vectors **
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    //define 10 x 20
+    vector<vector<int>>vec(10,vector<int>(20,0));
+    vec.push_back(vector<int>(20,0));
+    vec[2].push_back(1);
+
+
+    vector<int>arr[4];
+    arr[1].push_back(0);
+
+
+
+    //10 x 20 x 30  // int arr[10][20][30]
+    vector<vector<vector<int>>>vec(10,vector<vector<int>>(20,vector<int>(30,0)));
+
+
+    
+
+
+
+
+    return 0;
+}
